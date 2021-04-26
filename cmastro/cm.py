88fa3b029ea023b76a41071a977bb0529c5f55e2
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-04-26 17:01:42 -0400

    plots

--- cmastro/cm.py
+++ cmastro/cm.py
@@ -7,7 +7,7 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 
 
-data_path = pathlib.Path(__file__).resolve() / 'cmaps'
+data_path = pathlib.Path(__file__).resolve().parent / 'cmaps'
 
 # Colormap data files:
 cmap_files = data_path.glob('[!.]*')
@@ -19,7 +19,7 @@ for cmap_file in cmap_files:
         kw['delimiter'] = ','
 
     rgb_data = np.loadtxt(cmap_file, **kw)
-    cmap_name = f"cma_{cmap_file.stem}"
+    cmap_name = f"cma:{cmap_file.stem}"
 
     cmaps[cmap_name] = ListedColormap(rgb_data, name=cmap_name)
     cmaps[f"{cmap_name}_r"] = ListedColormap(rgb_data[::-1],
@@ -30,21 +30,22 @@ for cmap_file in cmap_files:
 #
 
 # emph and unph (formerly center_emph and center_deemph)
-cmaps['emph'] = ListedColormap((cmaps['hesperia'].colors[::-1][:192] +
-                                cmaps['laguna'].colors[1:][64:]),
-                               name="emph")
-cmaps['emph_r'] = ListedColormap(cmaps['emph'].colors[::-1],
-                                 name="emph_r")
+trim = 64
+warm = cmaps['cma:hesperia'].colors[::-1][:256 - trim]
+cool = cmaps['cma:laguna'].colors[trim + 1:]
+colors = np.concatenate((warm,
+                         [0.5 * (warm[-1] + cool[0])],
+                         cool))
+cmaps['cma:emph'] = ListedColormap(colors, name="cma:emph")
+cmaps['cma:emph_r'] = ListedColormap(colors[::-1], name="cma:emph_r")
 
-cmaps['unph'] = ListedColormap((cmaps['hesperia'].colors[:-1][64:] +
-                                cmaps['laguna'].colors[::-1][:192]),
-                               name="unph")
-cmaps['unph_r'] = ListedColormap(cmaps['emph'].colors[::-1],
-                                 name="unph_r")
+colors = np.concatenate((cmaps['cma:hesperia'].colors[:-1][trim:],
+                         cmaps['cma:laguna'].colors[::-1][:256-trim]))
+cmaps['cma:unph'] = ListedColormap(colors, name="cma:unph")
+cmaps['cma:unph_r'] = ListedColormap(colors[::-1],
+                                     name="cma:unph_r")
 
 
 for cmap in cmaps.values():
     cm.register_cmap(cmap=cmap)
 
-# make colormaps available to call
-locals().update(cmaps)
--- cmastro/plot.py
+++ cmastro/plot.py
@@ -6,21 +6,62 @@ import numpy as np
 from .cm import cmaps
 
 
-def plot_all_colormaps():
+def plot_all_colormaps(grayscale=False):
     """Plot all of the colormaps defined in this package"""
 
     grid = np.linspace(0, 1, 256)
 
     cmap_names = [name for name in cmaps if not name.endswith('_r')]
     nrows = len(cmap_names)
-    fig, axes = plt.subplots(nrows, 1, figsize=(6, 2*nrows))
+    fig, axes = plt.subplots(nrows, 1, figsize=(8, 2*nrows))
 
     for ax, name in zip(axes, cmap_names):
         cmap = cmaps[name]
-        H = cmap(grid).reshape(-1, 1)
-        ax.imshow(H, aspect=80)
+        rgb = cmap(grid)[:, :3]
+
+        if grayscale:
+            from colorspacious import cspace_converter
+            LAB = cspace_converter("sRGB1", "CAM02-UCS")(rgb)
+            L = LAB[:, 0] / 1e2
+            H = np.repeat(L[:, None], 3, axis=1)[None]
+        else:
+            H = rgb[None]
+
+        ax.imshow(H, aspect=40)
+        ax.set_title(name)
 
         # ax.text(x_text, y_text, cmap.name, va='center', ha='right')
+
+    # Turn off *all* ticks & spines, not just the ones with colormaps.
+    for ax in axes:
+        ax.set_axis_off()
+
+    return fig, axes
+
+
+def plot_lightness():
+    """Plot all of the colormaps as 1D lightness curves"""
+    from colorspacious import cspace_converter
+
+    grid = np.linspace(0, 1, 256)
+
+    cmap_names = [name for name in cmaps if not name.endswith('_r')]
+    nrows = len(cmap_names)
+
+    fig, axes = plt.subplots(nrows, 1, figsize=(8, 2*nrows),
+                             sharex=True, sharey=True)
+
+    for ax, name in zip(axes, cmap_names):
+        cmap = cmaps[name]
+        rgb = cmap(grid)[:, :3]
+
+        LAB = cspace_converter("sRGB1", "CAM02-UCS")(rgb)
+        L = LAB[:, 0] / 1e2
+
+        ax.plot(L)
+        ax.set_title(f"{name} (lightness)")
+
+    ax.set_ylim(0, 1)
 
     # Turn off *all* ticks & spines, not just the ones with colormaps.
     for ax in axes:
