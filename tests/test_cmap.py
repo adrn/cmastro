655c078eca8b74b5638c0c158ed9e1511add6d4d
Adrian Price-Whelan <583379+adrn@users.noreply.github.com>
2025-09-03 13:21:32 -0400

    fix

--- tests/test_cmap.py
+++ tests/test_cmap.py
@@ -5,11 +5,11 @@ import cmastro
 
 def test_plot():
     fig, ax = plt.subplots()
-    ax.plot([[0, 1], [2, 3]], cmap=cmastro.hesperia)
-    plt.clf(fig)
+    ax.imshow([[0, 1], [2, 3]], cmap=cmastro.hesperia)
+    plt.close(fig)
 
 
 def test_plot_str():
     fig, ax = plt.subplots()
-    ax.plot([[0, 1], [2, 3]], cmap="cma:hesperia")
-    plt.clf(fig)
+    ax.imshow([[0, 1], [2, 3]], cmap="cma:hesperia")
+    plt.close(fig)
